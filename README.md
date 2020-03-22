02b14ecf4d58f172e09d2ab5b2f102efb9236f94
Artem Poletsky <artempoletsky@gmail.com>
2020-03-22 16:55:20 +0500

    Skin modifier support Displace textures scale support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Scale with modifiers 
+# Scale with modifiers
 Blender addon which adds operator which applies scale to an object and its modifiers. For Blender 2.8+
 
 Demo: https://youtu.be/EdqKX0Vy6AM
@@ -10,11 +10,13 @@ Demo: https://youtu.be/EdqKX0Vy6AM
 
 ## Usage
 
-In object mode select objects needed to be edited. Press ctrl + A. Select `Apply scale with modifiers` option. 
+In object mode select objects needed to be edited. Press ctrl + A. Select `Apply scale with modifiers` option.
 
-For better results objects need to have even scale eg(0.5,0.5,0.5). For uneven objects warning will be displayed, and they will be deselected. 
+For better results objects need to have even scale eg(0.5,0.5,0.5). For uneven objects warning will be displayed, and they will be deselected.
 
-Multy user objects will be ignored by the script and deselected. 
+Multy user objects will be ignored by the script and deselected.
+
+For procedural displace textures, depends on mapping type and texture type issues are possible. For more predictable results use UV mapping type. 
 
 After script finish you can hide selected objects, and you will get only objects with possible issues.
 
@@ -26,8 +28,9 @@ You can disallow script to affect some modifiers by hiding them in the viewport.
 2. Screw - Screw offset
 3. Solidify- Thickness
 4. Wireframe - Thickness
-5. Displace - Strength
+5. Displace - Strength, Scale of procedural textures for displace modifier
 6. Shrinkwwrap - Offset
+7. Skin - radius of vertices
 
 You can support me on Gumroad, if you like my work. 
 https://gum.co/MwiQk
--- scale_with_modifiers.py
+++ scale_with_modifiers.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Scale with modifiers",
     "author": "Artem Poletsky",
-    "version": (1, 0, 1),
+    "version": (1, 1, 0),
     "blender": (2, 82, 0),
     "location": "Object > Apply > Apply scale with modifiers",
     "description": "Adds operator which applies scale to an object and its modifiers",
@@ -20,16 +20,46 @@ MODS = {
     'MIRROR': {'merge_threshold'},
     'SOLIDIFY': {'thickness'},
     'WIREFRAME': {'thickness'},
-    'DISPLACE': {'strength'},
+    'DISPLACE':  'function',
     'SHRINKWRAP': {'offset'},
     'WELD': {'merge_threshold'},
+    'SKIN': 'function',
 }
 
-def funcARRAY(mod, scale):
+def objectsSelectSet(objects, value):
+    for o in objects:
+        o.select_set(value)
+    
+
+def funcARRAY(mod, scale, object):
     mod.constant_offset_displace[0] *= scale
     mod.constant_offset_displace[1] *= scale
     mod.constant_offset_displace[2] *= scale
     mod.merge_threshold *= scale
+    return False
+    
+def funcDISPLACE(mod, scale, object):
+    mod.strength *= scale
+    if bool(mod.texture) & ((mod.texture_coords == "LOCAL") | (mod.texture_coords == "OBJECT")):
+        if hasattr(mod.texture, "noise_scale"):
+            mod.texture.noise_scale *= scale
+        return "objects have displace texture applied."
+    else: 
+        return False
+
+ 
+def funcSKIN(mod, scale, object):
+    verts = object.data.skin_vertices[0].data
+    for v in verts:
+        v.radius[0] *= scale
+        v.radius[1] *= scale
+
+    
+    
+def scaleSkinObjects(C, objects):
+    for o in objects:
+        scaleSkinObject(c, o)
+    
     
 
 class ScaleWithModifiersOperator(bpy.types.Operator):
@@ -43,10 +73,14 @@ class ScaleWithModifiersOperator(bpy.types.Operator):
 
     def execute(self, context):
         
+        objects = context.selected_objects
+        
         notEven = []
         clones = []
         
-        for obj in context.selected_objects:
+        funcWarnings = [];
+        
+        for obj in objects:
             if not obj.data:
                 continue
             
@@ -69,27 +103,44 @@ class ScaleWithModifiersOperator(bpy.types.Operator):
                 
                 if (mod.type in MODS) & mod.show_viewport:
                     if MODS[mod.type] == 'function':
-                        globals()["func" + mod.type](mod, scale)
+                        result = globals()["func" + mod.type](mod, scale, obj)
+                        if result:
+                            funcWarnings.append((obj, result))
                     else:    
                         for attrname in MODS[mod.type]:
                             val = getattr(mod, attrname)
                             setattr(mod, attrname, val * scale)
         
         warningMessage = "";
+        
+        warningObjects = []
+        if len(funcWarnings):
+            keys = {}
+            for obj, w in funcWarnings:
+                warningObjects.append(obj)
+                
+                if not w in keys: 
+                    keys[w] = 1
+                else: 
+                    keys[w] += 1
+                    
+            for k in keys:
+                warningMessage += ("{:d} " + k + " ").format(keys[k])
+        
         clonesLen = len(clones)
         if clonesLen:
              warningMessage += "{:d} objects are multi-user, ignoring ".format(clonesLen)
-             for o in clones:
-                 o.select_set(False)
+             objectsSelectSet(clones, False)
                  
         notEvenLen = len(notEven)
         if notEvenLen:
-            warningMessage += "Scale of {:d} objects is not even. Some issues are possible ".format(notEvenLen)
+            warningMessage += "Scale of {:d} objects is not even. ".format(notEvenLen)
         if warningMessage:
-            self.report({'WARNING'}, warningMessage)
+            self.report({'WARNING'}, warningMessage + "Some issues are possible ")
         bpy.ops.object.transform_apply(scale=True, location=False, rotation=False)
-        for o in notEven:
-                 o.select_set(False)
+        objectsSelectSet(notEven, False)
+        objectsSelectSet(warningObjects, False)
+        
         return { 'FINISHED' }
     
 
